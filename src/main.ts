1143476cf0bac54d5cb89003a6c70d4162addae1
Gabriel Ferrara <88694869+gabsferrara@users.noreply.github.com>
2023-08-23 21:43:56 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import Customer from "./domain/customer/entity/customer";
-import Address from "./domain/customer/value-object/adress";
+// import Customer from "./domain/customer/entity/customer";
+// import Address from "./domain/customer/value-object/adress";
 
 
-let customer = new Customer("123", "Gabriel Ferrara");
-const address = new Address("Rua dois", 2, "12345-678", "São Paulo");
-customer.Address = address;
-customer.activate();
+// let customer = new Customer("123", "Gabriel Ferrara");
+// const address = new Address("Rua dois", 2, "12345-678", "São Paulo");
+// customer.Address = address;
+// customer.activate();
 
 // const item1 = new OrderItem("1","Item 1", 10);
 // const item2 = new OrderItem("2","Item 2", 15);
